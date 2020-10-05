92ff0261dbe469105f18097ca1f87534b5e9772c
Tristan Helmich <fadenb@utzutzutz.net>
2020-10-05 17:45:42 +0000

    automed ESLint fixes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-var sdk = require("matrix-js-sdk");
+const sdk = require('matrix-js-sdk');
 
 try {
   const homeserver = core.getInput('homeserver');
@@ -9,7 +8,6 @@ try {
   const token = core.getInput('token');
   const message = core.getInput('message');
   const messagetype = core.getInput('messagetype');
-
 
   // Debug output
   console.log(`homeserver: ${homeserver}`);
@@ -20,27 +18,26 @@ try {
 
   // Create client object
   const client = sdk.createClient({
-    baseUrl: "https://matrix.org",
-    accessToken: token
+    baseUrl: 'https://matrix.org',
+    accessToken: token,
   });
 
   // Join channel (if we are already in the channel this does nothing)
-  client.joinRoom(channel).then(function() {
-    console.log("Joined channel");
+  client.joinRoom(channel).then(() => {
+    console.log('Joined channel');
   });
 
   // Send message
   const content = {
-    "msgtype": messagetype,
-    "body": message
+    msgtype: messagetype,
+    body: message,
   };
 
-  client.sendEvent(channel, "m.room.message", content, "").then((res) => {
+  client.sendEvent(channel, 'm.room.message', content, '').then(() => {
   // message sent successfully
   }).catch((err) => {
     console.log(err);
   });
-
 } catch (error) {
   core.setFailed(error.message);
 }
